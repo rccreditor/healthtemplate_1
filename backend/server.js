b9bb58e2517bf0891a8362a3d29087b4df139686
Your Name <you@example.com>
2025-06-25 19:51:32 +0530

    dot

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); // Must be at the top
+require("dotenv").config(); 
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
